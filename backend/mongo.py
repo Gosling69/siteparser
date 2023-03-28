0f0933adf9aad5779987d48081ae8c9b3f72efe0
dev-groz <groza4ka@gmail.com>
2023-03-28 18:58:08 +0700

    Removed case from get_items where there is only given one of arguments

--- backend/mongo.py
+++ backend/mongo.py
@@ -236,8 +236,9 @@ def update_our_item(entry: dict ) -> dict:
 
 @database_connector
 def get_items(init_date: str = None, end_date: str = None) -> list:
+
     #if init_date and end_date empty
-    if init_date is None and end_date is None:
+    if init_date is None or end_date is None:
         pipeline = [
             {
                 "$lookup":
@@ -267,124 +268,68 @@ def get_items(init_date: str = None, end_date: str = None) -> list:
         return result
 
 
+    #if given init_date and end_date
     try:
-        #if only given end_date
-        if init_date is None:
-            pipeline = [
-            {
-                u"$match": {}
-            }, 
-            {
-                u"$project": {
-                    u"_id": 1,
-                    u"name": u"$name",
-                    u"item_link": u"$item_link",
-                    u"site": u"$site",
-                    u"data": {
-                        u"$filter": {
-                            u"input": u"$data",
-                            u"as": u"data",
-                            u"cond": {
-                                u"$lte": [
-                                    u"$$data.date_time",
-                                    datetime.strptime(f"{end_date}", "%Y-%m-%d")
-                                ]
-                            }
-                        }
-                    }
-                }
-            }
-            ]
-
-        #if only given init_date
-        elif end_date is None:
-            pipeline = [
-            {
-                u"$match": {}
-            }, 
-            {
-                u"$project": {
-                    u"_id": 1,
-                    u"name": u"$name",
-                    u"item_link": u"$item_link",
-                    u"site": u"$site",
-                    u"data": {
-                        u"$filter": {
-                            u"input": u"$data",
-                            u"as": u"data",
-                            u"cond": {
-                                u"$gte": [
-                                    u"$$data.date_time",
-                                    datetime.strptime(f"{init_date}", "%Y-%m-%d")
-                                ]
-                            }
-                        }
-                    }
-                }
-            }
-            ]
-
-        #if given init_date and end_date
-        else:
-            pipeline = [
-            {
-                "$lookup":
-                    {
-                        "from": "site",
-                        "localField": "site",
-                        "foreignField": "_id",
-                        "as": "site"
-                    }
-            },
-            { "$unwind": { "path": "$site" } },
-            {
-                "$project": 
+        
+        pipeline = [
+        {
+            "$lookup":
                 {
-                    "_id": 1,
-                    "name": 1,
-                    "item_link": 1,
-                    # "last_price": 1,
-                    # "last_quantity":1,
-                    "site.name": 1,
-                    "site._id":1,
-                    "data":1,
+                    "from": "site",
+                    "localField": "site",
+                    "foreignField": "_id",
+                    "as": "site"
                 }
-            },
+        },
+        { "$unwind": { "path": "$site" } },
+        {
+            "$project": 
             {
-                u"$match": {}
-            }, 
-            {
-                u"$project": {
-                    u"_id": 1,
-                    u"name": u"$name",
-                    u"item_link": u"$item_link",
-                    u"site": u"$site",
-                    u"data": {
-                        u"$filter": {
-                            u"input": u"$data",
-                            u"as": u"data",
-                            u"cond": {
-                                u"$and": [
-                                    {
-                                        u"$gte": [
-                                            u"$$data.date_time",
-                                            datetime.strptime(f"{init_date}", "%Y-%m-%d")
-                                        ]
-                                    },
-                                    {
-                                        u"$lte": [
-                                            u"$$data.date_time",
-                                            datetime.strptime(f"{end_date}", "%Y-%m-%d")
-                                        ]
-                                    }
-                                ]
-                            }
-                        }
-                    },
-                    u"_id": 1
-                }
+                "_id": 1,
+                "name": 1,
+                "item_link": 1,
+                # "last_price": 1,
+                # "last_quantity":1,
+                "site.name": 1,
+                "site._id":1,
+                "data":1,
             }
-            ]
+        },
+        {
+            u"$match": {}
+        }, 
+        {
+            u"$project": {
+                u"_id": 1,
+                u"name": u"$name",
+                u"item_link": u"$item_link",
+                u"site": u"$site",
+                u"data": {
+                    u"$filter": {
+                        u"input": u"$data",
+                        u"as": u"data",
+                        u"cond": {
+                            u"$and": [
+                                {
+                                    u"$gte": [
+                                        u"$$data.date_time",
+                                        datetime.strptime(f"{init_date}", "%Y-%m-%d")
+                                    ]
+                                },
+                                {
+                                    u"$lte": [
+                                        u"$$data.date_time",
+                                        datetime.strptime(f"{end_date}", "%Y-%m-%d")
+                                    ]
+                                }
+                            ]
+                        }
+                    }
+                },
+                u"_id": 1
+            }
+        }
+        ]
     
         result = loads(dumps(Item.objects().aggregate(pipeline)))
     except ValueError:
